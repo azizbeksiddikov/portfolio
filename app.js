8124d5a8f46da3ee9ccd43b9c79f187e212300f8
Azizbek Siddikov <azbeksid@gmail.com>
2024-11-29 01:29:15 +0900

    feat: netlify fix minor issues

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
+const path = require("path");
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN.replace(/["';]/g, "");
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID.replace(/["';]/g, "");
 
-app.set("views", "views");
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Read projects data
