81cdf513441afb83751688d9e7b6e92084f0df65
Azizbek Siddikov <azbeksid@gmail.com>
2024-11-29 01:00:04 +0900

    Fix function dependencies and configuration

--- functions/server.js
+++ functions/server.js
@@ -1,5 +1,5 @@
 // netlify/functions/server.js
 const serverless = require("serverless-http");
-const app = require("../../app"); // Adjust the path to your app.js
+const app = require("../app");
 
 module.exports.handler = serverless(app);
